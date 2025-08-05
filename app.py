9309c602060e30082073fd921f585f1ad88f651d
Rakshith592 <rakshithsriv@gmail.com>
2025-08-05 21:38:33 +0530

    Update app.py
    added port

--- app.py
+++ app.py
@@ -170,4 +170,5 @@ def index():
     return render_template('index.html', BMI=[], recommendations=[])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        port = int(os.environ.get("PORT", 5000)) # Default to 5000 for local testing
+        app.run(debug=True, host='0.0.0.0', port=port)
